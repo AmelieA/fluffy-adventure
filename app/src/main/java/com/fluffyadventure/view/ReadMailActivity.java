cbcdfd0832641025283116370e0c2084869388de
jeremyedert <jeremy.edert@insa-lyon.fr>
2015-03-03 11:31:00 +0100

    Lecture d'un mail et liste d'amis commencées

---
+++ app/src/main/java/com/fluffyadventure/view/ReadMailActivity.java
@@ -0,0 +1,64 @@
+package com.fluffyadventure.view;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.fluffyadventure.fluffyadventure.MailBox;
+import com.fluffyadventure.view.R;
+
+public class ReadMailActivity extends Activity {
+
+    TextView textMailSender;
+    TextView textMailBody;
+    Button btnMailBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_mail);
+
+        textMailBody=(TextView) findViewById(R.id.TextMailBody);
+
+        textMailSender=(TextView) findViewById(R.id.TextMailSender);
+
+        btnMailBox=(Button) findViewById(R.id.BtnInBox);
+        btnMailBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReadMailActivity.this, MailBox.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_read_mail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
