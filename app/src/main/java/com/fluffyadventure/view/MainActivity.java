e90a1f1eba36076f9bf545ca06ecbfe478eca144
jeremyedert <jeremy.edert@insa-lyon.fr>
2015-03-04 16:10:28 +0100

    Correction d'un mauvais changement du bouton Login

--- app/src/main/java/com/fluffyadventure/view/MainActivity.java
+++ app/src/main/java/com/fluffyadventure/view/MainActivity.java
@@ -68,7 +68,7 @@ public class MainActivity extends Activity {
                 @Override
                 public void onClick(View v) {
                     if (Controller.getServer() != null){
-                        Intent intent = new Intent(MainActivity.this, ReadMailActivity.class);
+                        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                         startActivity(intent);
                     }
                     else {
