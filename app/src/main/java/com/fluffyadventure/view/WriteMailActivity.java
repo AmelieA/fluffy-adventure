a0508a3f12ffd614f24365d64bf30fe8e578f163
jeremyedert <jeremy.edert@insa-lyon.fr>
2015-03-16 15:33:22 +0100

    suppression de methodes liées à l'action bar (non utilisée dans notre appli) dans plusieurs activities

--- app/src/main/java/com/fluffyadventure/view/WriteMailActivity.java
+++ app/src/main/java/com/fluffyadventure/view/WriteMailActivity.java
@@ -81,29 +81,6 @@ public class WriteMailActivity extends Activity {
         mailBody.requestFocus();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_write_mail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onBackPressed() {
         Intent intent = new Intent(WriteMailActivity.this, FriendListActivity.class);
         startActivity(intent);
